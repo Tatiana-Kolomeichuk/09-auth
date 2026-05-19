e4774bf4885066975ffc1caf6cf8f931fe981b19
Tatiana <tany663@gmail.com>
2026-05-19 17:48:27 +0300

    change header and styles

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,7 +2,6 @@ import Link from "next/link";
 import css from "./Header.module.css";
 import AuthNavigation from "../AuthNavigation/AuthNavigation";
 
-
 export default function Header() {
   return (
     <header className={css.header}>
@@ -15,7 +14,7 @@ export default function Header() {
             <Link href="/">Home</Link>
           </li>
           <li>
-            <Link href="/notes/filter/all">Notes</Link>
+            <Link href={`/notes/filter/all`}>Notes</Link>
           </li>
           <AuthNavigation />
         </ul>
